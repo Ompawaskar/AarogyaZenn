ac1d9d6d366bf90f98b59feb25fb2a0614534324
yvrjgithub <127379082+yvrjgithub@users.noreply.github.com>
2024-03-26 18:35:54 +0530

    Update yoga.py

--- yoga.py
+++ yoga.py
@@ -3,18 +3,10 @@ import customtkinter as ctk
 from customtkinter import *
 from tkinter import ttk
 from PIL import Image,ImageTk
-import urllib.request
 import yogadata
-import io
-from elevenlabs import save
-from elevenlabs import stream
-from elevenlabs import play
-from elevenlabs.client import ElevenLabs
-from pygame import mixer
 
-client = ElevenLabs(
-  api_key="9345b521180c7cd759cc931c8cab763c", # Defaults to ELEVEN_API_KEY
-)
+
+from pygame import mixer
 
 def update_label_text(text):
     label_text = ""
@@ -23,47 +15,25 @@ def update_label_text(text):
     else:
         label_text = text
     return label_text
-def download_image(url, filename):
-    """Downloads the image from the URL and saves it locally (optional)."""
-    try:
-        response = urllib.request.urlopen(url)
-        with open(filename, 'wb') as f:
-            f.write(response.read())
-        return filename  # Return the filename if downloaded locally
-    except Exception as e:
-        print(f"Error downloading image: {e}")
-        return None  # Return None on download failure
+
 
 def display_image_from_url(item,url, window, width=None, height=None):
     """Displays the image from the URL directly in the CustomTkinter window."""
-    try:
-        response = urllib.request.urlopen(url)
-        image_data = response.read()
-
+  
         
-        image = Image.open(io.BytesIO(image_data))
-
+    image = ctk.CTkImage(light_image=Image.open("./yimages/"+str(no)+".png"),
+                                  dark_image=Image.open("./yimages/"+str(no)+".png"),
+                                  size=(280,280))
      
-        if width and height:
-            image = image.resize((width, height))  
-
-      
-        photo_image = ImageTk.PhotoImage(image)
-
-       
-        image_label = CTkButton(window, image=photo_image,text="",corner_radius=15,hover=False,fg_color="#ffefd6",command=lambda item=item: seedetails(item),height=350,width=320)
+    image_label = CTkButton(window, image=image,text="",corner_radius=15,hover=False,fg_color="#ffefd6",command=lambda item=item: seedetails(item),height=350,width=320)
         
-        image_label.image = photo_image
-        return image_label
+    return image_label
 
-    except Exception as e:
-        print(f"Error displaying image: {e}")
-
-def create_data_frame(item,row_index, column_index):
+def create_data_frame(item,row_index, column_index,no):
     frame = ctk.CTkFrame(master=f1,width=500,height=500,fg_color="transparent")
     frame.grid(row=row_index, column=column_index,padx =40,pady=20)
 
-    image = display_image_from_url(item,item['url_png'], frame, width=350, height=350)
+    image = display_image_from_url(item,no, frame, width=350, height=350)
     image.pack()
     ltext = update_label_text(item["sanskrit_name_adapted"])
     l2 = ctk.CTkLabel(master=image, text=ltext,font=("TkFixedFont", 22),text_color="#111111",bg_color="transparent")  
@@ -89,17 +59,6 @@ def seedetails(item):
     path = os.getcwd()
     p = os.path.join(path,"audios",filename)
 
-    audio = client.generate(
-    text=item["pose_description"],
-    voice="Emily",
-    model="eleven_multilingual_v2"
-    )
-
-    if os.path.exists(p):
-        return
-    else:
-        save(audio,p)
-    
     imagef = ctk.CTkScrollableFrame(master=new_frame,width=350,height=700,fg_color="#cec5f9",scrollbar_button_color="#cec5f9",scrollbar_button_hover_color="#cec5f9")
     imagef.pack(side =RIGHT,pady = 20)
     image = display_image_from_url(item,item['url_png'], imagef, width=350, height=350)
@@ -126,14 +85,16 @@ def seedetails(item):
     close_button.pack(side = RIGHT,padx=2)
     titlef = ctk.CTkScrollableFrame(master=new_frame,width=950,height=700,fg_color="#cec5f9",scrollbar_button_color="#cec5f9",scrollbar_button_hover_color="#cec5f9")
     titlef.pack(side =LEFT)
-    titleframe = CTkFrame(master=titlef,width=950,height=100,fg_color="#cec5f9")
-    titleframe.pack(padx =20,fill = "x",expand =True)
-    name1 = ctk.CTkLabel(master = titleframe,text=item["sanskrit_name_adapted"],font=("TkFixedFont", 60),bg_color="transparent")
-    name1.pack(side =LEFT,pady =20)
-    name2 = ctk.CTkLabel(master = titleframe,text=" - "+item["english_name"],font=("TkFixedFont", 60),bg_color="transparent",text_color="#777777")
-    name2.pack(side =LEFT,pady =20)
+    titleframe1 = CTkFrame(master=titlef,width=950,height=100,fg_color="#cec5f9")
+    titleframe1.pack(padx =20,fill = "x",expand =True)
+    name1 = ctk.CTkLabel(master = titleframe1,text=item["sanskrit_name_adapted"],font=("TkFixedFont", 60),bg_color="transparent")
+    name1.pack(side =LEFT)
+    titleframe2 = CTkFrame(master=titlef,width=950,height=100,fg_color="#cec5f9")
+    titleframe2.pack(padx =20,fill = "x",expand =True)
+    name2 = ctk.CTkLabel(master = titleframe2,text="("+item["english_name"]+")",font=("TkFixedFont", 35),bg_color="transparent",text_color="#777777")
+    name2.pack(side =LEFT)
     descframe = CTkFrame(master=titlef,width=950,height=100,fg_color="#cec5f9")
-    descframe.pack(padx =30,fill = "x",expand =True)
+    descframe.pack(padx =30,fill = "x",expand =True,pady =20)
     desc = ctk.CTkLabel(master = descframe,text="Instruction:",font=("TkFixedFont", 35),bg_color="transparent")
     desc.pack(side = LEFT)
     desccframe = CTkFrame(master=titlef,width=950,height=100,fg_color="#cec5f9")
@@ -169,14 +130,15 @@ root.grid_rowconfigure(0, weight=1)
 num_rows = (len(yogadata.data) // 4) + (len(yogadata.data) % 4 > 0)
 row_counter = 0
 column_counter = 0
-
+no = 1
 for item in yogadata.data:
    
-    data_frame = create_data_frame(item, row_counter, column_counter)
+    data_frame = create_data_frame(item, row_counter, column_counter,no)
     column_counter += 1
     if column_counter == 3:  # Reset column counter after 4 columns
         column_counter = 0
         row_counter += 1
+    no = no + 1
     
    
 
